7ae61338e9b272aad9973535ffc6fe431f236c37
Nithish SS <nithish@example.com>
2026-01-12 15:40:03 +0530

    update

--- frontend/src/CoursePlayer.tsx
+++ frontend/src/CoursePlayer.tsx
@@ -605,11 +605,7 @@ const CoursePlayer = () => {
     return url.replace("/view", "/preview");
   };
 
-  const getYoutubeId = (url: string) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    const match = url.match(regExp);
-    return (match && match[2].length === 11) ? match[2] : null;
-  };
+  
 
   const plyrOptions = useMemo(() => ({
     controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
